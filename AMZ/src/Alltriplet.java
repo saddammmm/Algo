c8257bda07f030fce73b2d4bfcecbb2fcd498a21
saddammmm <care.sdm@gmail.com>
2019-06-12 15:13:50 +0530

    sss

--- AMZ/src/Alltriplet.java
+++ AMZ/src/Alltriplet.java
@@ -1,17 +1,24 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class Alltriplet {
 
-	public static void main(String[]argd) throws IOException{
+	public static void main(String[]argd) throws IOException, ParseException{
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yy");
+		String d1="25 DEC 2018";
+		
+		Date d=new Date("03/12/2018");
+		
+		Date d3=sdf.parse(d1);
+		System.out.println(d3.before(d));
 		
 		
-		String loginURL="https://netbanking.hdfcbank.com/netbanking/entry?fldpwdtmp=&fldAppId=RS&fldDevicePrint=&fldTxnId=RGN&fldScrnSeqNbr=01&fldLangId=eng&fldDeviceId=01&fldWebServerId=YG&fldAppServerId=ZZ&fldRandomNumber=&fldRefPage=rsloginhtml&fldRefVal=USERID--NETBANKING--&fldLoginUserId=USERID";
-		
-		loginURL=loginURL.replaceAll("USERID", "60991273");
-		System.out.println(loginURL);
 		
 	}
 	}
